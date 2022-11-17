6746f592370f9c1cd82f32d52556b151b0e2cb89
Floyd Zweydinger <floyd.zweydinger@rub.de>
2022-11-17 13:14:53 +0100

    added main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+
+#ifndef CONFIG_SET
+#define CONFIG_SET
+
+#include <iostream>
+
+#define BENCHMODE
+constexpr uint32_t n=1;
+constexpr uint32_t r=15;
+constexpr uint32_t m=32;
+constexpr uint32_t threads=1;
+constexpr uint32_t s= uint32_t(1) << 5;
+constexpr uint32_t t= uint32_t(1) << 5;
+constexpr uint32_t BENCHES=10;
+constexpr uint32_t BENCHESMULT=100000;
+#endif //CONFIG_SET
